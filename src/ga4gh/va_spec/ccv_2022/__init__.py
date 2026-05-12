82fea93df9417d1aa753413b3080b6ef8d40b8e4
Kori Kuzma <korikuzma@gmail.com>
2026-05-12 13:08:02 -0400

    feat: add helper function for deriving onco code attrs (#51)
    close #48
    
    * Given onco evidence code, will return associated evidence outcome,
    strength, and score

--- src/ga4gh/va_spec/ccv_2022/__init__.py
+++ src/ga4gh/va_spec/ccv_2022/__init__.py
@@ -1,11 +1,19 @@
 """Module to load and init namespace at package level."""
 
+from .derived_evidence import (
+    CODE_PREFIX_TO_SCORE_MAP,
+    CODE_SUFFIX_TO_STRENGTH_MAP,
+    derive_onco_evidence_attributes,
+)
 from .models import (
     VariantOncogenicityEvidenceLine,
     VariantOncogenicityStatement,
 )
 
 __all__ = [
+    "CODE_PREFIX_TO_SCORE_MAP",
+    "CODE_SUFFIX_TO_STRENGTH_MAP",
+    "derive_onco_evidence_attributes",
     "VariantOncogenicityEvidenceLine",
     "VariantOncogenicityStatement",
 ]
